af0a6965715b1d5af8cbb83b5c4994829ff21544
emmagras <emma@mapbox.com>
2016-06-29 13:19:23 -0400

    add travis

---
+++ index.js
@@ -0,0 +1,10 @@
+/* @flow */
+'use strict';
+
+// # stats-online
+//
+// An tool for on line descriptive statistics calculastions
+
+var so = module.exports = {};
+
+so.incrementer = require('./lib/incrementer'); 
