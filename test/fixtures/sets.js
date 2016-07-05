0d376ad4cc37731ed098b22df15012dd1bae668c
emmagras <emma@mapbox.com>
2016-07-05 13:20:54 -0400

    change allow for aggregating variances

--- test/fixtures/sets.js
+++ test/fixtures/sets.js
@@ -1,5 +1,49 @@
 module.exports = {
-  'partA': [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-  'partB': [11, 12,13,14,15, 16, 17, 18, 19, 20],
-  'pooledAverage': 10
+  'precalculatedSets': [
+    {
+      'partA': [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+      'partB': [11, 12,13,14,15, 16, 17, 18, 19, 20, 21],
+      'pooledMean': 10,
+      'pooledVariance': 38.5,
+      'pooledStandardDeviation': 6.2048368229954285,
+      'individualVariances': {
+        'partA': 11,
+        'partB': 9.1666666666666
+      },
+      'individualMeans': {
+        'partA': 5,
+        'partB': 16
+      }
+    },
+    {
+      'partA': [1, 2, 3],
+      'partB': [4, 5, 6],
+      'pooledMean': 3.5,
+      'pooledVariance': 3.5,
+      'pooledStandardDeviation': 1.8708286933869707,
+      'individualVariances': {
+        'partA': 2/3,
+        'partB': 2/3
+      },
+      'individualMeans': {
+        'partA': 2,
+        'partB': 5
+      }
+    },
+    {
+      'partA': [1, 2, 3, 4, 5],
+      'partB': [6, 7, 8, 9],
+      'pooledMean': 5,
+      'pooledVariance': 7.5,
+      'pooledStandardDeviation': 2.7386127875258306,
+      'individualVariances': {
+        'partA': 2.5,
+        'partB': 1.6666666666666
+      },
+      'individualMeans': {
+        'partA': 3,
+        'partB': 7.5
+      }
+    },
+  ]
 };
--- test/offlineAverage.test.js
+++ test/offlineAverage.test.js
@@ -5,11 +5,14 @@ var fixture = require('./fixtures/sets')
 
 test('pooled mean', function (t) {
   var store = new Mean();
-  var avgPartA = store.offlineAverage(fixture.partA);
-  t.equal(avgPartA, 5);
+  for (var i = 0; i < fixture.precalculatedSets.length; i++) {
+    var partA = fixture.precalculatedSets[i].partA;
+    var partB = fixture.precalculatedSets[i].partB;
+    var avgPartA = store.offlineAverage(partA);
+    t.equal(avgPartA, fixture.precalculatedSets[i].individualMeans.partA);
 
-  var avgPartB = store.offlineAverage(fixture.partB);
-  t.equal(avgPartB, 15.5);
-
+    var avgPartB = store.offlineAverage(partB);
+    t.equal(avgPartB, fixture.precalculatedSets[i].individualMeans.partB);
+  }
   t.end();
 });
